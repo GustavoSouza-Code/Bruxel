c02a146af36e58be604b272460dd6a43a866024e
GustavoSouza-Code <contato.gustavosouza.rs@gmail.com>
2026-09-24 20:50:05 -0300

    feat: Esqueleto de Crud de usuário montado, aguardando DB

--- backend/src/service/userService.ts
+++ backend/src/service/userService.ts
@@ -1,55 +1,57 @@
-import { UserRole, User, CreateUserDTO } from "../models/user";
+import { UserRole, User, CreateUserDTO, UpdateUserDTO } from "../models/user";
 
 export class UserService {
+  async create(user: CreateUserDTO) {
+    const name = user.name;
+    const email = user.email;
+    const password = user.password;
 
-  async create (user: CreateUserDTO) {
-        const name = user.name
-        const email = user.email
-        const password = user.password
-        const cpf = user.cpf
+    // Debug
+    console.log("Dados Recebidos em userService.create:");
 
-        // Debug
-        console.log("Dados Recebidos em userService.create:")
-
-    
-        // Validadores
-        if (!name) {
-            throw new Error("O nome é obrigatório")
-            console.log("Nome não informado")
-        }
-    
-        if (!email) {
-            console.log("Email não informado")
-            throw new Error("O email é obrigatório")
-        }
-    
-        if (!password) {
-            console.log("Senha não informado")
-            throw new Error("A senha é obrigatória")
-        }
-    
-        if (!cpf) {
-            console.log("CPF não informado")
-            throw new Error("O CPF é obrigatório")
-        }
-
-        // Debug
-        console.log("Nome:", name)
-        console.log("Email:", email)
-        console.log("Senha:", password)
-        console.log("CPF:", cpf)
-    
-        const newUser: User = {
-            id:"1",
-            name,
-            email,
-            password,
-            cpf,
-            role: UserRole.USER
-        }
-    
-        console.log("Usuário criado com sucesso:", newUser)
-        return newUser
+    // Validadores
+    if (!name) {
+      console.log("Nome não informado");
+      throw new Error("O nome é obrigatório");
     }
 
+    if (!email) {
+      console.log("Email não informado");
+      throw new Error("O email é obrigatório");
+    }
+
+    if (!password) {
+      console.log("Senha não informado");
+      throw new Error("A senha é obrigatória");
+    }
+
+    // Debug
+    console.log("Nome:", name);
+    console.log("Email:", email);
+    console.log("Senha:", password);
+
+    const newUser: User = {
+      id: "1",
+      name,
+      email,
+      password,
+      role: UserRole.USER,
+    };
+
+    console.log("Usuário criado com sucesso:", newUser);
+    return newUser;
+  }
+
+  async getAll() {
+    // Debug
+    console.log("Busca de usuarios - GetAll");
+
+    //return await this.userRepository.findAll();
+  }
+  async update(id: string, data: UpdateUserDTO) {
+    // Debug
+    console.log("Atualizando usuario", id, data);
+  }
+
+  async delete(id: string) {}
 }
